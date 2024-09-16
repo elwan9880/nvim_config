03950e296a103ebbd81e6ec659bab2adddbcbb95
Chun-Tse Shao <elwan9880@gmail.com>
2024-09-16 16:21:53 -0700

    Update nvim-lspconfig.lua

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,7 +2,7 @@ return {
   'neovim/nvim-lspconfig',
   config = function()
     require('lspconfig').clangd.setup {
-      cmd = {"/usr/bin/clangd", "--compile-commands-dir=/usr/local/google/home/ctshao"}
+      cmd = {"/usr/bin/clangd", "--compile-commands-dir=${COMPILE_COMMANDS_DIR}"}
     }
   end
 }
