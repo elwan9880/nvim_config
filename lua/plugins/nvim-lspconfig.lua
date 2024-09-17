450d3cee955ba6dd11700bccd998d22f9424a846
Chun-Tse Shao <elwan9880@gmail.com>
2024-09-17 16:24:48 -0700

    Update nvim-lspconfig.lua

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,7 +2,29 @@ return {
   'neovim/nvim-lspconfig',
   config = function()
     require('lspconfig').clangd.setup {
-      cmd = {"/usr/bin/clangd", "--compile-commands-dir=${COMPILE_COMMANDS_DIR}"}
+      cmd = {"/usr/bin/clangd", "--compile-commands-dir=/usr/local/google/home/ctshao"}
     }
-  end
+
+    vim.api.nvim_set_keymap('n', '<leader>dd', '', {
+      noremap = true,
+      silent = true,
+      callback = function()
+        if vim.diagnostic.is_disabled() then
+          vim.diagnostic.enable()
+        else
+          vim.diagnostic.disable()
+        end
+      end,
+      desc = "Toggle diagnostics",
+    })
+
+    vim.api.nvim_set_keymap('n', '<leader>dn', '', {
+      noremap = true,
+      silent = true,
+      callback = function()
+        vim.diagnostic.goto_next()
+      end,
+      desc = "Goto next diagnostic",
+    })
+  end,
 }
