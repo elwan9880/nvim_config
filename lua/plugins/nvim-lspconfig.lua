8cdf5583fcd102a7caf57c523309b9b2e56971f0
Chun-Tse Shao <elwan9880@gmail.com>
2025-09-20 15:52:00 -0700

    Update nvim-lspconfig.lua

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,34 +1,41 @@
 return {
-  'neovim/nvim-lspconfig',
+  "neovim/nvim-lspconfig",
   config = function()
-    lspconfig = require('lspconfig')
-    lspconfig.clangd.setup ({
-        cmd = {"/usr/bin/clangd", "--compile-commands-dir=/usr/local/google/home/ctshao"},
-        on_attach = function(client, bufnr)
-            vim.bo[bufnr].formatexpr = nil
-        end,
+    vim.lsp.config("clangd", {
+      capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      on_attach = function(client, bufnr)
+        vim.diagnostic.enable(false)
+      end,
     })
 
-    vim.api.nvim_set_keymap('n', '<leader>dd', '', {
+    vim.lsp.enable({"clangd"})
+
+    vim.api.nvim_set_keymap("n", "<leader>dd", "", {
       noremap = true,
       silent = true,
       callback = function()
-        if vim.diagnostic.is_disabled() then
-          vim.diagnostic.enable()
+        if vim.diagnostic.is_enabled() then
+          vim.diagnostic.enable(false)
         else
-          vim.diagnostic.disable()
+          vim.diagnostic.enable()
         end
       end,
       desc = "Toggle diagnostics",
     })
 
-    vim.api.nvim_set_keymap('n', '<leader>dn', '', {
+    vim.api.nvim_set_keymap("n", "<leader>dn", "", {
       noremap = true,
       silent = true,
       callback = function()
-        vim.diagnostic.goto_next()
+        vim.diagnostic.jump({count=1})
       end,
       desc = "Goto next diagnostic",
     })
+
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Goto Definition' })
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Goto Declaration' })
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Goto References' })
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover Docs' })
+
   end,
 }
